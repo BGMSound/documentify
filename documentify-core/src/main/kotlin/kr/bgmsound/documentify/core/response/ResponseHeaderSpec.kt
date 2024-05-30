1d6a015710379c4feba4eec17a2cc44979c91bad
Gyumin Bae <bgmsound0528@gmail.com>
2024-05-30 15:03:24 +0900

    fix: response header 선언 오류 해결

--- documentify-core/src/main/kotlin/kr/bgmsound/documentify/core/response/ResponseHeaderSpec.kt
+++ documentify-core/src/main/kotlin/kr/bgmsound/documentify/core/response/ResponseHeaderSpec.kt
@@ -2,17 +2,17 @@ package kr.bgmsound.documentify.core.response
 
 import kr.bgmsound.documentify.core.HeaderSpec
 import org.springframework.restdocs.headers.HeaderDocumentation
-import org.springframework.restdocs.headers.RequestHeadersSnippet
+import org.springframework.restdocs.headers.ResponseHeadersSnippet
 import org.springframework.restdocs.snippet.Snippet
 
 class ResponseHeaderSpec : HeaderSpec() {
-    private var headersSnippet: RequestHeadersSnippet? = null
+    private var headersSnippet: ResponseHeadersSnippet? = null
 
     override fun build(): List<Snippet> {
         if (headers.isEmpty()) {
             return emptyList()
         }
-        headersSnippet = HeaderDocumentation.requestHeaders(headers.map { it.descriptor })
+        headersSnippet = HeaderDocumentation.responseHeaders(headers.map { it.descriptor })
         return listOf(headersSnippet!!)
     }
 }
