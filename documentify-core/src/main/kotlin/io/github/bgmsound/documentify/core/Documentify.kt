0549944ccc7d380fd272060b82973b7afe7f15cf
Gyumin Bae <bgmsound0528@gmail.com>
2024-07-08 18:35:41 +0900

    refactor: standalone 방식으로 구조 변경

--- documentify-core/src/main/kotlin/io/github/bgmsound/documentify/core/Documentify.kt
+++ documentify-core/src/main/kotlin/io/github/bgmsound/documentify/core/Documentify.kt
@@ -4,27 +4,24 @@ import io.github.bgmsound.documentify.core.specification.DocumentSpec
 import io.restassured.module.mockmvc.RestAssuredMockMvc.given
 import io.restassured.module.mockmvc.specification.MockMvcRequestSpecification
 
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.restdocs.RestDocumentationContextProvider
 import org.springframework.restdocs.RestDocumentationExtension
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration
-import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.context.WebApplicationContext
+import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder
 
 @ExtendWith(RestDocumentationExtension::class)
 abstract class Documentify {
     protected lateinit var spec: MockMvcRequestSpecification
 
-    @BeforeEach
-    fun setUp(
-        webApplicationContext: WebApplicationContext,
-        restDocumentation: RestDocumentationContextProvider
+    fun standAloneSetup(
+        restDocumentation: RestDocumentationContextProvider,
+        vararg controller: Any
     ) {
         val mockMvc = MockMvcBuilders
-            .webAppContextSetup(webApplicationContext)
-            .apply<DefaultMockMvcBuilder>(documentationConfiguration(restDocumentation))
+            .standaloneSetup(*controller)
+            .apply<StandaloneMockMvcBuilder>(documentationConfiguration(restDocumentation))
             .build()
         spec = given().mockMvc(mockMvc)
     }
