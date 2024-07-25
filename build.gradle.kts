6874f10c1038fee55c1f0dd09c17d341c2496ce9
Gyumin Bae <bgmsound0528@gmail.com>
2024-07-25 10:08:15 +0900

    chore: 빌드 구성 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm")
-    kotlin("plugin.spring")
-    id("io.spring.dependency-management")
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
+    id("io.spring.dependency-management") version "1.1.5"
 }
 
 group = "io.github.bgmsound"
@@ -14,6 +14,18 @@ subprojects {
     version = rootProject.version
     apply(plugin = "kotlin")
     apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "io.spring.dependency-management")
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencyManagement {
+        imports {
+            mavenBom("org.springframework.boot:spring-boot-dependencies:3.3.1")
+        }
+    }
+
     tasks {
         withType<KotlinCompile> {
             kotlinOptions {
--- documentify-core/build.gradle.kts
+++ documentify-core/build.gradle.kts
@@ -3,14 +3,14 @@ plugins {
 }
 
 dependencies {
-    implementation(spring.test)
-    implementation(spring.restdocs.mockmvc)
-    implementation(spring.restdocs.assured)
-    implementation(lib.rest.assured)
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.1")
-    implementation("com.epages:restdocs-api-spec-mockmvc:0.18.2")
-    implementation("com.epages:restdocs-api-spec-restassured:0.18.2")
+    implementation("org.springframework.boot:spring-boot-starter-test")
+    api("org.springframework.restdocs:spring-restdocs-mockmvc")
+    api("org.springframework.restdocs:spring-restdocs-restassured")
+    api("io.rest-assured:spring-mock-mvc:5.5.0")
+    api("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+    api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.17.1")
+    api("com.epages:restdocs-api-spec-mockmvc:0.18.2")
+    api("com.epages:restdocs-api-spec-restassured:0.18.2")
 }
 
 publishing {
