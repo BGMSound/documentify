a7efcefc2a9b35822c0c17f4fd89fe75e83822ec
Gyumin Bae <bgmsound0528@gmail.com>
2024-08-01 11:42:15 +0900

    refactor: 코드 클린업

--- documentify-core/src/main/kotlin/io/github/bgmsound/documentify/core/specification/request/RequestHeaderSpec.kt
+++ documentify-core/src/main/kotlin/io/github/bgmsound/documentify/core/specification/request/RequestHeaderSpec.kt
@@ -1,10 +1,32 @@
 package io.github.bgmsound.documentify.core.specification.request
 
 import io.github.bgmsound.documentify.core.specification.HeaderSpec
+import io.github.bgmsound.documentify.core.specification.element.Header
+import io.github.bgmsound.documentify.core.specification.element.SpecElement.Requirement
 import org.springframework.restdocs.headers.HeaderDocumentation
 import org.springframework.restdocs.snippet.Snippet
 
 class RequestHeaderSpec : HeaderSpec() {
+    fun bearer(path: String, description: String, sample: String): Header {
+        val header = Header.newHeader(
+            path,
+            description,
+            "Bearer $sample",
+            Requirement.REQUIRED
+        )
+        return header(header)
+    }
+
+    fun bearer(description: String, sample: String): Header {
+        val header = Header.newHeader(
+            "Authorization",
+            description,
+            "Bearer $sample",
+            Requirement.REQUIRED
+        )
+        return header(header)
+    }
+
     override fun build(): List<Snippet> {
         if (headers.isEmpty()) {
             return emptyList()
