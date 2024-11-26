22fc4d5b72f569643e072ede3ae25980a1e6cd46
Gyumin Bae <bgmsound0528@gmail.com>
2024-11-26 19:30:27 +0900

    refactor: remove redundant qualifier

--- documentify-core/src/main/kotlin/io/github/bgmsound/documentify/core/documentation/element/parameter/QueryParameter.kt
+++ documentify-core/src/main/kotlin/io/github/bgmsound/documentify/core/documentation/element/parameter/QueryParameter.kt
@@ -1,29 +1,28 @@
 package io.github.bgmsound.documentify.core.documentation.element.parameter
 
-import io.github.bgmsound.documentify.core.documentation.element.SpecElement
 import org.springframework.restdocs.request.ParameterDescriptor
 import org.springframework.restdocs.request.RequestDocumentation
 import org.springframework.restdocs.snippet.Attributes
 
 class QueryParameter(
-    descriptor: ParameterDescriptor
+    descriptor: ParameterDescriptor,
 ) : Parameter(descriptor) {
     companion object {
         fun newQueryParameter(
             key: String,
             description: String,
             sample: String,
-            requirement: SpecElement.Requirement
+            requirement: Requirement,
         ): QueryParameter {
             val descriptor = RequestDocumentation.parameterWithName(key)
                 .description(description)
                 .attributes(
-                    Attributes.Attribute(SpecElement.SAMPLE_KEY, sample)
+                    Attributes.Attribute(SAMPLE_KEY, sample)
                 )
             when (requirement) {
-                SpecElement.Requirement.REQUIRED -> {}
-                SpecElement.Requirement.OPTIONAL -> descriptor.optional()
-                SpecElement.Requirement.IGNORED -> descriptor.ignored()
+                Requirement.REQUIRED -> {}
+                Requirement.OPTIONAL -> descriptor.optional()
+                Requirement.IGNORED -> descriptor.ignored()
             }
             return QueryParameter(descriptor)
         }
