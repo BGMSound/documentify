deeb51ab0563750ccbe56e03cd76f4249a7ae7e6
Gyumin Bae <bgmsound0528@gmail.com>
2025-03-25 22:51:01 +0900

    Refactor maven publish script to streamline publishing configuration

--- documentify-core/build.gradle.kts
+++ documentify-core/build.gradle.kts
@@ -1,12 +1,3 @@
-import com.vanniktech.maven.publish.SonatypeHost
-import java.time.Year
-
-plugins {
-    alias(libs.plugins.gradle.mavenCentral.publish)
-    signing
-    `maven-publish`
-}
-
 dependencies {
     implementation(libs.spring.boot.starter.test)
     implementation(libs.restdocs.api.spec)
@@ -15,62 +6,4 @@ dependencies {
     compileOnly(libs.spring.boot.starter.webflux)
     api(libs.jackson.databind)
     api(libs.jackson.datatype.jsr310)
-}
-
-signing {
-    val gpgSecret = project.findProperty("gpg.secret").toString()
-    val gpgPassphrase = project.findProperty("gpg.passphrase").toString()
-
-    useInMemoryPgpKeys(gpgSecret, gpgPassphrase)
-    sign(publishing.publications)
-}
-
-mavenPublishing {
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-
-    val projectGroup = property("project.group").toString()
-    val projectName = property("project.name").toString()
-    val projectVersion = property("project.version.id").toString()
-    val projectDescription = property("project.description").toString()
-    val projectUrl = property("project.url").toString()
-    val projectUrlScm = property("project.url.scm").toString()
-    val projectLicense = property("project.license").toString()
-    val projectLicenseUrl = property("project.license.url").toString()
-    val projectDeveloperId = property("project.developer.id").toString()
-    val projectDeveloperName = property("project.developer.name").toString()
-    val projectDeveloperEmail = property("project.developer.email").toString()
-    val projectDeveloperUrl = property("project.developer.url").toString()
-
-    coordinates(
-        groupId = projectGroup,
-        artifactId = "$projectName-core",
-        version = projectVersion
-    )
-
-    pom {
-        name = projectName
-        description = projectDescription
-        inceptionYear = "${Year.now().value}"
-        url = projectUrl
-        licenses {
-            license {
-                name = projectLicense
-                url = projectLicenseUrl
-                distribution = projectUrl
-            }
-        }
-        developers {
-            developer {
-                id = projectDeveloperId
-                name = projectDeveloperName
-                email = projectDeveloperEmail
-                url = projectDeveloperUrl
-            }
-        }
-        scm {
-            url = projectUrlScm
-            connection = "scm:git:git://github.com/${projectDeveloperId}"
-            developerConnection = "scm:git:ssh://git@github.com/${projectDeveloperId}"
-        }
-    }
 }
