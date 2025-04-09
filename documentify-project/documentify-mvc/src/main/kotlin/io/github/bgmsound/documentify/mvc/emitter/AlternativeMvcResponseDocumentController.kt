2040bc9b3426b7cfb51fad0ecf8aa599907b7437
Gyumin Bae <bgmsound0528@gmail.com>
2025-04-09 15:08:09 +0900

    Refactor alternative response document controller to simplify response handling

--- documentify-project/documentify-mvc/src/main/kotlin/io/github/bgmsound/documentify/mvc/emitter/AlternativeMvcResponseDocumentController.kt
+++ documentify-project/documentify-mvc/src/main/kotlin/io/github/bgmsound/documentify/mvc/emitter/AlternativeMvcResponseDocumentController.kt
@@ -6,39 +6,36 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/**")
 class AlternativeMvcResponseDocumentController private constructor(
-    private val status: Int,
-    private val response: Any
+    status: Int,
+    response: Any
 ) {
+    private val response = if (response is ResponseEntity<*>) {
+        response
+    } else ResponseEntity.status(status).body(response)
+
     @GetMapping
     fun get(): Any {
-        return response()
+        return response
     }
 
     @PostMapping
     fun post(): Any {
-        return response()
+        return response
     }
 
     @PutMapping
     fun put(): Any {
-        return response()
+        return response
     }
 
     @DeleteMapping
     fun delete(): Any {
-        return response()
+        return response
     }
 
     @PatchMapping
     fun patch(): Any {
-        return response()
-    }
-
-    private fun response(): Any {
-        if (response is ResponseEntity<*>) {
-            return response
-        }
-        return ResponseEntity.status(status).body(response)
+        return response
     }
 
     companion object {
