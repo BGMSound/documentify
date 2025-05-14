2be5aea46b0d0ad47724692f8d8a03f53a7851f6
Gyumin Bae <bgmsound0528@gmail.com>
2025-05-14 15:31:13 +0900

    Unify the common validation logic for DocumentResult into the abstract layer from its subclasses.

--- documentify-project/documentify-core/src/main/kotlin/io/github/bgmsound/documentify/core/emitter/AbstractDocumentResult.kt
+++ documentify-project/documentify-core/src/main/kotlin/io/github/bgmsound/documentify/core/emitter/AbstractDocumentResult.kt
@@ -2,9 +2,13 @@ package io.github.bgmsound.documentify.core.emitter
 
 import io.github.bgmsound.documentify.core.specification.element.field.Field
 import io.github.bgmsound.documentify.core.specification.schema.response.ResponseSpec
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers
 
 abstract class AbstractDocumentResult : DocumentResult {
-    abstract fun validateJsonPath(jsonResultMatcher: JsonResultMatcher)
+    abstract fun expect(jsonPath: String, matcher: Matcher<*>)
+
+    abstract fun expectValue(jsonPath: String, value: Any)
 
     override fun validateWith(responseSpec: ResponseSpec) {
         val matchers = aggregateMatchers(responseSpec.fields)
@@ -12,7 +16,18 @@ abstract class AbstractDocumentResult : DocumentResult {
             return
         }
         matchers.forEach { matcher ->
-            validateJsonPath(matcher)
+            val jsonPath = matcher.jsonPath
+            val value = matcher.expectedValue
+            if (jsonPath.endsWith("[*]")) {
+                if (value !is List<*>) {
+                    throw IllegalArgumentException("sample value type must be List")
+                }
+                expect(jsonPath.substringBeforeLast("[*]"), Matchers.containsInAnyOrder(*value.toTypedArray()))
+            } else if (jsonPath.contains("[*]") && !jsonPath.endsWith("[*]")) {
+                expect(jsonPath, Matchers.hasItem(value))
+            } else {
+                expectValue(jsonPath, value)
+            }
         }
     }
 
--- documentify-project/documentify-reactive/src/main/kotlin/io/github/bgmsound/documentify/reactive/emitter/ReactiveDocumentResult.kt
+++ documentify-project/documentify-reactive/src/main/kotlin/io/github/bgmsound/documentify/reactive/emitter/ReactiveDocumentResult.kt
@@ -3,26 +3,19 @@ package io.github.bgmsound.documentify.reactive.emitter
 import io.github.bgmsound.documentify.core.emitter.AbstractDocumentResult
 import io.github.bgmsound.documentify.core.emitter.JsonResultMatcher
 import io.github.bgmsound.documentify.core.specification.schema.response.ResponseSpec
+import org.hamcrest.Matcher
 import org.hamcrest.Matchers
 import org.springframework.test.web.reactive.server.WebTestClient
 
 class ReactiveDocumentResult(
     private val actualResponse: WebTestClient.BodyContentSpec
 ) : AbstractDocumentResult() {
-    override fun validateJsonPath(jsonResultMatcher: JsonResultMatcher) {
-        val jsonPath = jsonResultMatcher.jsonPath
-        val value = jsonResultMatcher.expectedValue
+    override fun expect(jsonPath: String, matcher: Matcher<*>) {
+        actualResponse.jsonPath(jsonPath).value(matcher)
+    }
 
-        if (jsonPath.endsWith("[*]")) {
-            if (value !is List<*>) {
-                throw IllegalArgumentException("sample value type must be List")
-            }
-            actualResponse.jsonPath(jsonPath.substringBeforeLast("[*]")).value(Matchers.containsInAnyOrder(*value.toTypedArray()))
-        } else if (jsonPath.contains("[*]") && !jsonPath.endsWith("[*]")) {
-            actualResponse.jsonPath(jsonPath).value(Matchers.hasItem(value))
-        } else {
-            actualResponse.jsonPath(jsonPath).value(Matchers.equalToObject(value))
-        }
+    override fun expectValue(jsonPath: String, value: Any) {
+        actualResponse.jsonPath(jsonPath).value(Matchers.equalToObject(value))
     }
 
     companion object {
