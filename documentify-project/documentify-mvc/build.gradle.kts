6f72d136cc630fedc29f01da41019c625d4aa677
Gyumin Bae <bgmsound0528@gmail.com>
2026-03-29 21:02:12 +0900

    Change spring-restdocs-webtestclient dependency from implementation to compileOnly

--- documentify-project/documentify-mvc/build.gradle.kts
+++ documentify-project/documentify-mvc/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
     compileOnly(libs.spring.boot.starter.web)
-    implementation(libs.spring.restdocs.webtestclient)
+    compileOnly(libs.spring.restdocs.webtestclient)
     implementation(libs.restdocs.api.spec)
 }
